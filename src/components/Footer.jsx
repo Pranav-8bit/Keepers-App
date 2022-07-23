df0f696cce981a87ec5f4a3ecf2899903283082b
Pranav tiwari <67794060+Pranav-8bit@users.noreply.github.com>
2022-07-23 23:15:48 +0530

    Keepers App using ReactJS

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Footer() {
+  //   const time = new Date();
+  //   const Year1 = time.getFullYear;
+  return (
+    <footer>
+      <p>ⓒ Copyright {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+export default Footer;
